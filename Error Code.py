3118b5df78fa4049c45f626cfdcf44251b5511c6
HetanshThakkar <hetanshthakkar69@gmail.com>
2026-02-24 21:31:31 +0530

    Add files via upload

---
+++ Error Code.py
@@ -0,0 +1,21 @@
+print("Prime Range Analyzer")
+start = int(input("Enter start: "))
+end = int(input("Enter end: "))
+num = start
+prime_count = 0
+while num <= end:
+i = 2
+flag = 0
+while i < num:
+   if num % i == 0:
+     flag = 1
+  i = i + 1
+if flag == 0:
+    print("Prime:", num)
+    prime_count = prime_count + 1
+    num = num + 1
+print("Total primes:", prime_count
+if prime_count > 10:
+   print("Many primes")
+else:
+   print("Few primes")
