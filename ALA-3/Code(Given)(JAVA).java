2f47cb59b2aea2aa81290820bfcdd7b8b5e207fb
HetanshThakkar <hetanshthakkar69@gmail.com>
2026-04-30 18:57:56 +0530

    Create Code(Given)(JAVA).java

---
+++ ALA-3/Code(Given)(JAVA).java
@@ -0,0 +1,9 @@
+public class Main {
+ public static void main(String[] args) {
+ double a=10, b=5;
+ System.out.println("Add: "+(a+b));
+ System.out.println("Sub: "+(a-b));
+ System.out.println("Mul: "+(a*b));
+ System.out.println("Div: "+(a/b));
+ }
+}
